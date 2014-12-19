100ef2682fb737a146412b51cb8ace444d511e29
Faiz Shukri <faizshukri90@gmail.com>
2014-12-19 22:57:50 +0000

    Import Date

--- src/flight/Examples.java
+++ src/flight/Examples.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Date;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
